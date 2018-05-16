263d063767f9ac2d24d8931d4962fcb8265ee043
mf11y <curonicu@gmail.com>
2018-05-16 07:26:22 -0400

    Allow input in drawn squares

---
+++ Grid/Grid/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Grid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel1.Location = new System.Drawing.Point(56, 89);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(360, 360);
+            this.panel1.TabIndex = 0;
+            this.panel1.Click += new System.EventHandler(this.P1_Click);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Grid/Grid/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grid
+{
+    public partial class Form1 : Form
+    {
+        int mouseClickX;
+        int mouseClickY;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+            //this.ResizeRedraw = true;
+            Graphics gr = e.Graphics;
+            Pen myPen = new Pen(Brushes.Black, 1);
+            Font myFont = new Font("Arial", 10);
+            int NumLines = 10;
+
+            int x = 0;
+            int y = 0;
+            int spacing = 40;
+
+            int linelength = spacing * 9;
+
+            for (int i = 0; i < NumLines; i++)
+            {
+                if (i % 3 == 0 && i != 0)
+                    myPen.Width = 3;
+                else
+                    myPen.Width = 1;
+
+                gr.DrawLine(myPen, x, y, x, linelength);
+                x += spacing;
+            }
+
+            x = 0;
+
+            for (int i = 0; i < NumLines; i++)
+            {
+
+                if (i % 3 == 0 && i != 0)
+                    myPen.Width = 3;
+                else
+                    myPen.Width = 1;
+
+                gr.DrawLine(myPen, x, y, linelength, y);
+                y += spacing;
+            }
+
+            
+        }
+
+        private void P1_Click(object sender, EventArgs e)
+        {
+            mouseClickX = panel1.PointToClient(Cursor.Position).X;
+            mouseClickY = panel1.PointToClient(Cursor.Position).Y;
+
+        }
+
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (mouseClickX != 0)
+            {
+                Graphics gr = panel1.CreateGraphics();
+                Pen myPen = new Pen(Brushes.Black, 1);
+                Font myFont = new Font("Arial", 10);
+
+                int drawX = (mouseClickX / 40) * 40;
+                int drawY = (mouseClickY / 40) * 40;
+
+                char number = (char)e.KeyValue;
+
+                if(number >= '0' && number <= '9' )
+                    gr.DrawString(number.ToString(), myFont, Brushes.Black, drawX + 14, drawY + 12);
+            }
+            mouseClickX = 0;
+            mouseClickY = 0;
+        }
+    }
+}
